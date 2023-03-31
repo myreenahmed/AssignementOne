61d1ade9c92077031bce7348648b8f15696497f0
Myreen Ahmed <ahmedmyreen@gmail.com>
2023-03-31 12:59:20 -0400

    My first commit

---
+++ CountCharacters/js/myscripts.js
@@ -0,0 +1,11 @@
+function countChar() {
+  let v = document.getElementById("characterinput").value;
+  // trimming the spaces
+  let len = v.trim().length;
+
+  document.getElementById("result").value = len;
+  //ensuring user inputs data
+  if (len <= 0) {
+    alert("Error!! Please enter your text!");
+  }
+}
